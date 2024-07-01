0738c8495b511daa07e58fb28ab1637703b13205
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2024-07-01 15:32:17 -0700

    Realized grammars were broken. Copy from original code.

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,23 @@
 
-const grammars = {
-  46: "adj-animate:8| |noun-animate:8| |verb-s:7| |number:6| |adj:9| |noun-concrete:8",
-  54: "adj-animate:10| |noun-animate:10| |verb-s:9| |number:6| |adj:10| |noun-concrete:10",
-  58: "adj-animate:8| |noun-animate:8| and |adj-animate:8| |noun-animate:8| |verb-s:8| |adj:9| |noun-concrete:9",
-  66: "adj-animate:9| |noun-animate:8| and |adj-animate:9| |noun-animate:8| |verb-s:8| |number:6| |adj:9| |noun-concrete-plural:9",
-  70: "adj-animate:9| |noun-animate:9| and |adj-animate:9| |noun-animate:9| |verb-s:8| |adj:9| |noun-concrete:9| in |place:8",
-  80: "adj-animate:9| |noun-animate:9| and |adj-animate:9| |noun-animate:9| |verb-s:8| |adj:9| |noun-concrete:9| and |adj:9| |noun-concrete:9"
+let grammars = {
+  // 44: "adj-animate:9| |noun-animate:9| |verb-s:8| |adj:9| |noun-concrete:9",
+  46: "adj-animate:8| |noun-animate:8| |verb-s:7| |number:6| |adj:9| |noun-concrete-plural:8",
+  // 48: "adj-animate:10| |noun-animate:9| |verb-s:9| |adj:10| |noun-concrete:10",
+  // 50: "adj-animate:9| |noun-animate:9| |verb-s:8| |number:6| |adj:9| |noun-concrete-plural:9",
+  // 52: "adj-animate:9| |noun-animate:9| |verb-s:9| |adj:10| |noun-concrete:9| in |place:7",
+  54: "adj-animate:10| |noun-animate:10| |verb-s:9| |number:6| |adj:10| |noun-concrete-plural:10",
+  // 56: "adj-animate:10| |noun-animate:10| |verb-s:9| |adj:10| |noun-concrete:10| in |place:8",
+  58: "adj-animate:8| |noun-animate:8| and |adj-animate:8| |noun-animate:8| |verb:8| |adj:9| |noun-concrete:9",
+  // 60: "adj-animate:9| |noun-animate:8| and |adj-animate:9| |noun-animate:8| |verb:8| |adj:9| |noun-concrete:9",
+  // 62: "adj-animate:9| |noun-animate:9| |verb-s:8| |adj:9| |noun-concrete:9| and |adj:9| |noun-concrete:9",
+  // 64: "adj-animate:8| |noun-animate:8| and |adj-animate:8| |noun-animate:8| |verb:8| |adj:9| |noun-concrete:8| in |place:7",
+  66: "adj-animate:9| |noun-animate:8| and |adj-animate:9| |noun-animate:8| |verb:8| |number:6| |adj:9| |noun-concrete-plural:9",
+  // 68: "adj-animate:9| |noun-animate:8| and |adj-animate:9| |noun-animate:8| |verb:8| |adj:9| |noun-concrete:9| in |place:8",
+  70: "adj-animate:9| |noun-animate:9| and |adj-animate:9| |noun-animate:9| |verb:8| |adj:9| |noun-concrete:9| in |place:8",
+  80: "adj-animate:9| |noun-animate:9| and |adj-animate:9| |noun-animate:9| |verb:8| |adj:9| |noun-concrete:9| and |adj:9| |noun-concrete:9",
+  // 90: "adj-animate:10| |noun-animate:9| and |adj-animate:10| |noun-animate:9| |verb:7| |adj:10| |noun-concrete:9| and |adj:10| |noun-concrete:9| in |place:7",
+  // 100: "adj-animate:10| |noun-animate:10| and |adj-animate:10| |noun-animate:10| |verb:9| |adj:10| |noun-concrete:10| and |number:6| |adj:9| |noun-concrete-plural:9| in |place:7",
+  // 110: "adj-animate:10| |noun-animate:10| and |adj-animate:10| |noun-animate:10| |verb:9| |number:8| |adj:10| |noun-concrete-plural:9| and |number:8| |adj:10| |noun-concrete-plural:9| in |place:7"
 };
 
 
