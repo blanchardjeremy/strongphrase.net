62b566000b7490cc717c1aeb2575253b650cf2bf
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2024-07-20 11:27:06 -0700

    Cleanup  styles more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { } from 'react';
 import PassphraseGenerator from './PassphraseGenerator.js';
 import './App.css';
+import { RiShieldKeyholeLine } from "react-icons/ri";
+import { HiOutlineChatBubbleOvalLeftEllipsis } from "react-icons/hi2";
+
 
 
 const App = () => {
@@ -15,9 +18,9 @@ const App = () => {
         <section className="content">
 
 
-          <div className="card flex flex-row bg-slate-100 shadow-xl mt-12 password-scheme">
+          <div className="card lg:card-side flex flex-row bg-slate-100 shadow-xl mt-12 password-scheme">
             <div className="flex-none w-1/2 relative card-body">
-              <h2 className="card-title">Use a passphrase for...</h2>
+              <h2 className="card-title"><HiOutlineChatBubbleOvalLeftEllipsis className="text-3xl" />Use a <u>passphrase</u> for...</h2>
               <p>Use randomly-generated pass<strong className="italic">phrase</strong> for each of your most important accounts that you type often:</p>
               <ul className="list-disc ml-8">
                 <li>Your master password for your password manager</li>
@@ -26,7 +29,7 @@ const App = () => {
                 <li>Wi-fi</li>
               </ul>
 
-              <p>Examples:</p>
+              <p className="example-label">Examples:</p>
               <ul className="list-disc ml-8">
                 <li><code>dynamic mutt eats 250 deformed worms</code></li>
                 <li><code>cruel major and vocal cop fashion petty bedbug</code></li>
@@ -36,15 +39,15 @@ const App = () => {
             <div className="border-r border-gray-200"></div>
 
             <div className="flex-grow card-body flex flex-col">
-              <div class="flex-grow"> 
-                <h2 className="card-title">And a password manager for everything else!</h2>
-                <p>Use a <strong>password manager</strong> with <strong>unqiue, randomly-generated</strong> passwords for everything else. (We suggest <a href="https://1password.com/" className="link" target="_blank" rel="noreferrer">1Password</a>).</p>
-                <p>Examples:</p>
+              
+                <h2 className="card-title"><RiShieldKeyholeLine className="text-3xl" />And a <u>password manager</u> for...</h2>
+                <p>Use a <strong>password manager</strong> with <strong>unqiue, randomly-generated</strong> passwords for everything else. It will generate random passwords for each website and automatically fill them for you each time you log-in, so you don't have to type them. (We suggest <a href="https://1password.com/" className="link" target="_blank" rel="noreferrer">1Password</a>).</p>
+                <p className="mb-2 example-label">Examples:</p>
                 <ul className="list-disc ml-8">
                   <li><code>7ZuNburjjGmme-MDuE*</code></li>
                   <li><code>3@Y@qXWb@LKnd7qCfsd</code></li>
                 </ul>
-              </div>
+              
               <div className="card-actions justify-end">
                 <a className="btn btn-secondary text-white" href="https://1password.com" target="_blank" rel="noreferrer">Get 1Password</a>
               </div>
