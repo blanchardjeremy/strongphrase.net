9323f4230ab15853b6026edf502340f815b32900
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2024-07-19 23:03:09 -0700

    Add suggestion to use password manager

--- src/App.js
+++ src/App.js
@@ -11,7 +11,19 @@ const App = () => {
         <p className="text-xl">Cryptographically Secure Passphrase Generator</p>
       </header>
       <PassphraseGenerator />
-      <section className="mt-8">
+      <section className="content">
+        <div className="card bg-slate-100 w-full shadow-xl mt-12">
+          <div className="card-body">
+            <h2 className="card-title">When to use passphrases</h2>
+            <p>Here is the password system we recommend</p>
+            <ol className="list-decimal ml-8">
+              <li>Use a strong, randomly-generated pass<strong class="italic">phrase</strong> (like the ones offered on this site) for each of your most important accounts that you type often: Your password manager, Google, Apple iCloud, laptop login</li>
+              <li>Use a <strong>password manager</strong> with <strong>unqiue, randomly-generated</strong> passwords for everything else. (We suggest <a href="https://1password.com/">1Password</a>.)</li>
+            </ol>
+          </div>
+        </div>
+      </section>
+      <section className="mt-8 content">
         <h2 className="text-2xl font-bold my-4">How does it work?</h2>
         <p>
           This website randomly selects words from predefined lists and arranges them into meaningful sentences.
@@ -27,7 +39,7 @@ const App = () => {
       </section>
       <footer className="mt-8">
         <p>This site collects no data. All interactions happen directly in your browser and stay entirely on your computer.</p>
-        <p>Find the <a href="https://github.com/blanchardjeremy/getapassphrase" className="text-blue-500 hover:underline">source code on GitHub</a>. You can also <a href="https://github.com/blanchardjeremy/getapassphrase/issues" className="text-blue-500 hover:underline">report a bug or offer feedback</a>. Original code by Ryan Foster.</p>
+        <p>Find the <a href="https://github.com/blanchardjeremy/getapassphrase">source code on GitHub</a>. You can also <a href="https://github.com/blanchardjeremy/getapassphrase/issues">report a bug or offer feedback</a>. Original code by Ryan Foster.</p>
       </footer>
     </div>
   );
