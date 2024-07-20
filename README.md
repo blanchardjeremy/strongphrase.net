0d958f576b365dfc51dac80e1d1887e19494fa6d
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2024-07-20 12:44:31 -0700

    Add examples to readme

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # StrongPhrase.net
 Use this site to create memorable, strong passphrases.
 
+This tool is similar to [diceeware](https://www.eff.org/dice), but generates phrases that are easier to visualize and remember.
+
+
+## Examples 
+
+| Diceware                                                          | **StrongPhrase.net**                                                                  | 
+|-------------------------------------------------------------------|---------------------------------------------------------------------------------------|
+| `shading deflected panorama` (40 bits of entropy)                 | `evil juror obtains 300 thin moths` (46 bits of entropy)                              |
+| `country cradle barbecue predator` (52 bits of entropy)           | `drunk niece and greedy goose clean tall book` (58 bits of entropy)                   |
+| `dyslexia glitter repossess glimpse unrobed` (64 bits of entropy) | `emotional boxer and concerned virus acquire 45 smashed baskets` (66 bits of entropy) |
+
+Entropy referrs to how many attempts would be required to guess the password.
+
+As you can see, StrongPassphrase.net passphrases must be longer to achieve the same amount of entropy as Diceware passphrases. But they are easier to remember.
+
 ## Credits
 This is a fork of [Get a Passphrase by Ryan Foster](https://github.com/openidauthority/getapassphrase) with significant 
--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const App = () => {
     <div className="font-body">
       <div className="max-w-screen-lg mx-auto px-4 py-8">
         <header className="mb-8">
-          <h1 className="text-4xl font-header mb-2">Get a Passphrase</h1>
-          <p className="text-xl">Cryptographically Secure Passphrase Generator</p>
+          <h1 className="text-4xl font-header mb-2">StrongPhrase.net</h1>
+          <p className="text-xl">Create a strong, memorable passphrases</p>
         </header>
         <PassphraseGenerator />
         <section className="content">
@@ -72,13 +72,14 @@ const App = () => {
       </div>
 
       <footer className="footer footer-center bg-neutral text-neutral-content p-10">
-        <aside>
+        <aside className="mb-0">
           <p>
-            <strong>Get a Passphrase</strong> - Create a strong, memorable passphrase
+            <strong>StrongPhrase.net</strong> - Create a strong, memorable passphrase to use as your master password
           </p>
           <p>This site collects no data. All interactions happen directly in your browser and stay entirely on your computer.</p>
+          <p>Originally created by Ryan Foster. Re-written by Jeremy Blanchard.</p>
         </aside>
-        <nav className="grid grid-flow-col gap-7">
+        <nav className="grid grid-flow-col gap-8 mt-0">
           <a className="link" href="https://github.com/blanchardjeremy/getapassphrase">Code on GitHub</a>
           <a className="link" href="https://github.com/blanchardjeremy/getapassphrase/issues">Submit feedback or a bug</a>
         </nav>
