67c2ae4766d8a2d91f0f33c7d4edaf5c37c33f8d
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2024-07-21 10:34:41 -0700

    Add markdown formatting to FAQ

--- src/Faq.js
+++ src/Faq.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+import { stripIndent } from 'common-tags';
 import './Faq.css';
 
 const FAQItem = ({ question, answer }) => {
   return (
     <div className="faq-item">
       <h2 className="faq-question">{question}</h2>
-      <div className="faq-answer">{answer}</div>
+      <ReactMarkdown
+        className="faq-answer"
+        children={answer}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+      />
     </div>
   );
 };
@@ -18,34 +27,33 @@ const Faq = () => {
       <div className="faq-container">
         <FAQItem 
           question="What is this tool? How does it help me?" 
-          answer={
-            <>
-              <p>Use this site to create strong passphrases that are easy to remember.</p>
-              <p><strong>Passphrases are much stronger</strong> than most passwords because they are randomly generated, whereas passwords are often reused, shorter, and easier to guess.</p>
-              <p>This site was inspired by <a href="https://www.eff.org/dice" className="link" target="_blank" rel="noreferrer">diceware</a>, but designed to generate sentences that are <strong>easier to visualize and remember</strong> than diceware phrases.</p>
-            </>
-          } 
+          answer={stripIndent`
+            Use this site to create strong passphrases that are easy to remember.
+
+            These passphrases are **much stronger** than most passwords because they are randomly generated, whereas passwords are often reused, shorter, and easier to guess.
+
+            This site was inspired by [diceware](https://www.eff.org/dice), but designed to generate sentences that are **easier to visualize and remember** than diceware phrases.
+          `} 
         />
         
         <FAQItem 
           question="How does this site generate passphrases?" 
-          answer={
-            <>
-              <p>This website randomly selects words from predefined lists and arranges them into meaningful sentences. These passphrases are much stronger than most humanly-generated passwords, but reasonably easy to memorize, making them ideal for protecting your most important accounts.</p>
-            </>
-          } 
+          answer={stripIndent`
+            This website randomly selects words from predefined lists and arranges them into meaningful sentences.
+            These passphrases are much stronger than most humanly-generated passwords, but reasonably easy to memorize, making them ideal for protecting your most important accounts.
+          `} 
         />
         
         <FAQItem 
           question="Wait, is it safe to generate a passphrase from a website?" 
-          answer={
-            <>
-              <p>Yes (relatively). This website runs entirely on your browser. There's no server that generates the password. You can turn off your wifi and the site will still work!</p>
-              <p>For the extra paranoid, you can generate a "<a href="https://www.eff.org/dice" className="link" target="_blank" rel="noreferrer">diceware</a>" passphrase entirely offline.</p>
-            </>
-          } 
+          answer={stripIndent`
+            Yes (relatively). This website runs entirely on your browser. There's no server that generates the password. You can turn off your wifi and the site will still work!
+
+            For the extra paranoid, you can generate a "[diceware](https://www.eff.org/dice)" passphrase entirely offline.
+          `} 
         />
         
+        {/* Add more FAQItem components here */}
       </div>
     </section>
   );
