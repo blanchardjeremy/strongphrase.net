a3e1677ead6da5417775758c46dd82d4e8e7ac5e
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2024-07-22 21:43:12 -0700

    Add button to show all entropy steps on table

--- src/EntropyCrackTable.js
+++ src/EntropyCrackTable.js
@@ -19,20 +19,25 @@ const formatNumber = (number) => {
 const EntropyCrackTimeTable = () => {
   const [hashRate, setHashRate] = useState(defaultHashRate);
   const [costPerGuess32, setCostPerGuess32] = useState(0.25);
+  const [showAllSteps, setShowAllSteps] = useState(false);
 
   const handleCostChange = (event) => {
-      setCostPerGuess32(Number(event.target.value));
+    setCostPerGuess32(Number(event.target.value));
+  };
+
+  const toggleSteps = () => {
+    setShowAllSteps(!showAllSteps);
   };
 
   const generateTableData = () => {
     let data = [];
-    for (let bits = 5; bits <= 100; bits += 5) {
+    let step = showAllSteps ? 1 : 5;
+    for (let bits = 5; bits <= 100; bits += step) {
       const numberOfGuesses = 2 ** bits;
-      // const avgCost = (numberOfGuesses) * (costPerGuess32/ (2 ** 32)) / 2 ;
       const avgCost = avgCostToCrack(bits, costPerGuess32);
       const roundedCost = formatDollarToScale(avgCost, 2, false);
       const avgTime = timeToCrackAvg(bits, hashRate);
-      const maxTime = avgTime*2;
+      const maxTime = avgTime * 2;
       data.push({
         bits,
         guesses: formatNumber(numberOfGuesses),
@@ -48,67 +53,76 @@ const EntropyCrackTimeTable = () => {
 
   return (
     <div className="container mx-auto p-4">
-        <div className="mb-6">
-          <p>
-            <Link className="link link-hover mr-4" to="/">← Go back home</Link>
-          </p>
+      <div className="mb-6">
+        <p>
+          <Link className="link link-hover mr-4" to="/">← Go back home</Link>
+        </p>
+      </div>
+
+      <h2 className="font-header text-3xl">Time and cost to crack</h2>
+
+      <div className="flex flex-row gap-4">
+        <HashRateSelector setHashRate={setHashRate} hashRate={hashRate} />
+
+        <div>
+          <label className="label label-text">$ Cost per 2^32 guesses:</label>
+          <select 
+            value={costPerGuess32} 
+            onChange={handleCostChange} 
+            className="select select-bordered w-auto max-w-xs select-sm"
+          >
+            {Object.entries(costOptions).map(([label, value]) => (
+              <option key={value} value={value}>{label}</option>
+            ))}
+          </select>
         </div>
 
-        <h2 className="font-header text-3xl">Time and cost to crack</h2>
-
-        <div className="flex flex-row gap-4">
-          <HashRateSelector setHashRate={setHashRate} hashRate={hashRate} />
-          
-          <div>
-            <label className="label label-text">$ Cost per 2^32 guesses:</label>
-            <select 
-                value={costPerGuess32} 
-                onChange={handleCostChange} 
-                className="select select-bordered w-auto max-w-xs select-sm"
-            >
-                {Object.entries(costOptions).map(([label, value]) => (
-                    <option key={value} value={value}>{label}</option>
-                ))}
-            </select>
-          </div>
+        <div>
+          <button 
+            onClick={toggleSteps} 
+            className="btn btn-sm justify-end"
+          >
+            {showAllSteps ? 'Show every 5 steps' : 'Show all entropy steps'}
+          </button>
         </div>
+      </div>
 
-        <div className="card card-body p-6 bg-gray-100 mt-4 flex flex-grow max-w-3xl text-sm">
-          <p className="mt-0">
-            The folks at 1Password estimate that it <a href="https://blog.1password.com/cracking-challenge-update/" className="link link-hover">costs $6 per 2<sup>32</sup> guesses</a> to crack their system. Their password hashing is much more complex than the hashing assumptions on the main page (bcrypt). bcrypt with simpler configurations, so the cost in our scenario is likely much less. And it is only going to go down in the future.
-          </p>
-        </div>
+      <div className="card card-body p-6 bg-gray-100 mt-4 flex flex-grow max-w-3xl text-sm">
+        <p className="mt-0">
+          The folks at 1Password estimate that it <a href="https://blog.1password.com/cracking-challenge-update/" className="link link-hover">costs $6 per 2<sup>32</sup> guesses</a> to crack their system. Their password hashing is much more complex than the hashing assumptions on the main page (bcrypt). bcrypt with simpler configurations, so the cost in our scenario is likely much less. And it is only going to go down in the future.
+        </p>
+      </div>
 
-        <div className="card card-body p-6 bg-green-100 mt-4 flex flex-grow max-w-3xl text-sm">
-          <p className="mt-0">
-            <strong>Takeaway:</strong> 45 bits should be your minimum and most people don't need more than 60 bits.
-          </p>
-        </div>
+      <div className="card card-body p-6 bg-green-100 mt-4 flex flex-grow max-w-3xl text-sm">
+        <p className="mt-0">
+          <strong>Takeaway:</strong> 45 bits should be your minimum and most people don't need more than 60 bits.
+        </p>
+      </div>
 
-        <div className="overflow-x-auto mt-8">
-            <table className="table table-zebra w-auto">
-              <thead>
-                <tr>
-                  <th>Bits</th>
-                  <th>Pool of possible guesses</th>
-                  <th>Avg. Crack Time</th>
-                  <th>Max Crack Time (double)</th>
-                  <th>Avg cost to crack</th>
-                </tr>
-              </thead>
-              <tbody>
-                {tableData.map((row) => (
-                  <tr key={row.bits}>
-                    <td>{row.bits}</td>
-                    <td>{row.guesses}</td>
-                    <td>{row.avgtime}</td>
-                    <td>{row.maxtime}</td>
-                    <td>{row.cost}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-        </div>
+      <div className="overflow-x-auto mt-8">
+        <table className="table table-zebra w-auto">
+          <thead>
+            <tr>
+              <th>Bits</th>
+              <th>Pool of possible guesses</th>
+              <th>Avg. Crack Time</th>
+              <th>Max Crack Time (double)</th>
+              <th>Avg cost to crack</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((row) => (
+              <tr key={row.bits}>
+                <td className="font-bold">{row.bits}</td>
+                <td>{row.guesses}</td>
+                <td>{row.avgtime}</td>
+                <td>{row.maxtime}</td>
+                <td>{row.cost}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
